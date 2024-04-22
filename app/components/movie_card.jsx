23d7d4c64accaaa85b673796a5417de5b5f3fc12
Sofia <sophia.shashova@yandex.ru>
2024-04-22 19:39:11 +0400

    Stub for card without posters added

--- app/components/movie_card.jsx
+++ app/components/movie_card.jsx
@@ -4,13 +4,15 @@ import "../styles/home.css";
 
 
 const MovieCard = ({ movie_info }) => {
+    console.log(movie_info)
     return (
         <div className="movie-card">
-            <div className="movie-image" > 
-            {(!movie_info.poster.url) ? (
-        <span>Нет постера</span>
-      ) : (
-            <img src={movie_info.poster.url} alt={movie_info.name} />)}
+            <div className="movie-image" >
+                {(!movie_info.poster || !movie_info.poster.url) ? (
+                    <span className="stub">Нет постера</span>
+                ) : (
+                    <img src={movie_info.poster.url} alt={movie_info.name} />
+                )}
             </div>
             <div className="movie-info">
                 <h2 className="movie-name">{movie_info.name}</h2>
@@ -18,11 +20,15 @@ const MovieCard = ({ movie_info }) => {
                     <p>{movie_info.countries.map(country => country.name).join(", ")} </p>
                     <p>{movie_info.year}</p>
                 </div>
-                <div className="imdb-rate">Рейтинг IMDb: {movie_info.rating.imdb}</div>
+                {/* <div className="imdb-rate">Рейтинг IMDb: {movie_info.rating.imdb}</div> */}
                 <div className="movie-genres">{movie_info.genres.map(genre => <span className="movie-genre"> {genre.name}</span>)}</div>
-
-                <div className={Number(movie_info.rating.kp) >= 8 ? "kinopoisk-rate green" : "kinopoisk-rate grey"}>{Math.round(Number(movie_info.rating.kp) * 100) / 100}</div>
-                <p className="movie-description">{movie_info.description}</p>
+                {(!movie_info.rating || !movie_info.rating.kp) ? (
+                    <div className="kinopoisk-rate grey">0</div>
+                ) : (
+                    <div className={Number(movie_info.rating.kp) >= 8 ? "kinopoisk-rate green" : "kinopoisk-rate grey"}>
+                        {Math.round(Number(movie_info.rating.kp) * 100) / 100}
+                    </div>
+                )}<p className="movie-description">{movie_info.description}</p>
             </div>
         </div>
     );
--- app/pages/home.jsx
+++ app/pages/home.jsx
@@ -13,15 +13,17 @@ export default function Home() {
   const [totalElements, setTotalElements] = useState(0);
   const [filters, setFilters] = useState({});
   const [searchText, setSearchText] = useState("");
-  const containerRef = useRef(null); 
+  const containerRef = useRef(null);
   const API_URL = 'https://api.kinopoisk.dev/v1.4/movie';
+  const [loading, setLoading] = useState(true);
 
 
   const getApiData = async () => {
+    setLoading(true);
     const queryParams = new URLSearchParams();
     queryParams.append('query', searchText);
     queryParams.append('limit', moviesPerPage);
-    queryParams.append('page', currentPage); 
+    queryParams.append('page', currentPage);
     for (const key in filters) {
       if (filters[key] !== "") {
         queryParams.append(key, filters[key]);
@@ -29,35 +31,36 @@ export default function Home() {
     }
     const queryString = queryParams.toString();
 
-    fetch(searchText? (`${API_URL}/search?${queryString}`
-) : `${API_URL}?${queryString}`, {
+    fetch(searchText ? (`${API_URL}/search?${queryString}`
+    ) : `${API_URL}?${queryString}`, {
       headers: {
         'X-API-KEY': process.env.TOKEN
       }
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         setMovies(data.docs);
-
         setTotalPages(data.pages);
-        setTotalElements(data.total)
-
+        setTotalElements(data.total);
+        setLoading(false);
       })
       .catch(e => {
         console.log(`Проверьте токен ${e}`);
+        setLoading(false);
       });
   };
-  console.log(totalPages)
   useEffect(() => {
     getApiData();
-  }, [currentPage, moviesPerPage, filters, searchText]);
+  }, [currentPage, filters, searchText]);  //delete from dependencies - moviesPerPage
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
     containerRef.current.scrollIntoView({ behavior: 'smooth' });
   };
 
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [moviesPerPage]); //переходит на первую страницу при обновлении количетсва фильмов на странице
 
   const handleFilterChange = (key, value) => {
     setFilters(prevState => ({
@@ -74,52 +77,51 @@ export default function Home() {
   // const filteredMovies = searchText? movies.filter(movie =>
   //       movie.name.toLowerCase().includes(searchText.toLowerCase())
   //      ) : movies;
-      console.log(movies)
 
   return (
-    <div ref={containerRef} className="home-container"> 
-    <Header searchText={searchText} onSearchInput={handleSearchInput} />
-    <div className="filters">
-            <Select defaultValue="" onChange={(value) => handleFilterChange('year', value)}>
-              <Select.Option value="">Все годы</Select.Option>
-              <Select.Option value="2023">2023</Select.Option>
-              <Select.Option value="2022">2022</Select.Option>
-              <Select.Option value="2021">2021</Select.Option>
-              <Select.Option value="2020">2020</Select.Option>
-              <Select.Option value="2019">2019</Select.Option>
-              <Select.Option value="2018">2018</Select.Option>
-              <Select.Option value="2017">2017</Select.Option>
-              <Select.Option value="2016">2016</Select.Option>
-              <Select.Option value="2015">2015</Select.Option>
-              <Select.Option value="2014">2014</Select.Option>
-              <Select.Option value="2013">2013</Select.Option>
-              <Select.Option value="2012">2012</Select.Option>
-              <Select.Option value="2011">2011</Select.Option>
-              <Select.Option value="2010">2010</Select.Option>
-            </Select>
-            <Select defaultValue="" onChange={(value) => handleFilterChange('countries.name', value)}>
-              <Select.Option value="">Все страны</Select.Option>
-              <Select.Option value="США">США</Select.Option>
-              <Select.Option value="Россия">Россия</Select.Option>
-              <Select.Option value="Великобритания">Великобритания</Select.Option>
-              <Select.Option value="Китай">Китай</Select.Option>
-              <Select.Option value="Франция">Франция</Select.Option>
-              <Select.Option value="Индия">Индия</Select.Option>
-              <Select.Option value="Германия">Германия</Select.Option>
-              <Select.Option value="СССР">СССР</Select.Option>
-              <Select.Option value="Канада">Канада</Select.Option>
-              <Select.Option value="Италия">Италия</Select.Option>
-            </Select>
-            <Select defaultValue="" onChange={(value) => handleFilterChange('ageRating', value)}>
-              <Select.Option value="">Все возрастные рейтинги</Select.Option>
-              <Select.Option value="0">0+</Select.Option>
-              <Select.Option value="6">6+</Select.Option>
-              <Select.Option value="12">12+</Select.Option>
-              <Select.Option value="16">16+</Select.Option>
-              <Select.Option value="18">18+</Select.Option>
-            </Select>
-          </div>
-      {(!movies.length) ? (
+    <div ref={containerRef} className="home-container">
+      <Header searchText={searchText} onSearchInput={handleSearchInput} />
+      <div className="filters">
+        <Select defaultValue="" onChange={(value) => handleFilterChange('year', value)}>
+          <Select.Option value="">Все годы</Select.Option>
+          <Select.Option value="2023">2023</Select.Option>
+          <Select.Option value="2022">2022</Select.Option>
+          <Select.Option value="2021">2021</Select.Option>
+          <Select.Option value="2020">2020</Select.Option>
+          <Select.Option value="2019">2019</Select.Option>
+          <Select.Option value="2018">2018</Select.Option>
+          <Select.Option value="2017">2017</Select.Option>
+          <Select.Option value="2016">2016</Select.Option>
+          <Select.Option value="2015">2015</Select.Option>
+          <Select.Option value="2014">2014</Select.Option>
+          <Select.Option value="2013">2013</Select.Option>
+          <Select.Option value="2012">2012</Select.Option>
+          <Select.Option value="2011">2011</Select.Option>
+          <Select.Option value="2010">2010</Select.Option>
+        </Select>
+        <Select defaultValue="" onChange={(value) => handleFilterChange('countries.name', value)}>
+          <Select.Option value="">Все страны</Select.Option>
+          <Select.Option value="США">США</Select.Option>
+          <Select.Option value="Россия">Россия</Select.Option>
+          <Select.Option value="Великобритания">Великобритания</Select.Option>
+          <Select.Option value="Китай">Китай</Select.Option>
+          <Select.Option value="Франция">Франция</Select.Option>
+          <Select.Option value="Индия">Индия</Select.Option>
+          <Select.Option value="Германия">Германия</Select.Option>
+          <Select.Option value="СССР">СССР</Select.Option>
+          <Select.Option value="Канада">Канада</Select.Option>
+          <Select.Option value="Италия">Италия</Select.Option>
+        </Select>
+        <Select defaultValue="" onChange={(value) => handleFilterChange('ageRating', value)}>
+          <Select.Option value="">Все возрастные рейтинги</Select.Option>
+          <Select.Option value="0">0+</Select.Option>
+          <Select.Option value="6">6+</Select.Option>
+          <Select.Option value="12">12+</Select.Option>
+          <Select.Option value="16">16+</Select.Option>
+          <Select.Option value="18">18+</Select.Option>
+        </Select>
+      </div>
+      {loading ? (
         <Flex align="center" gap="middle" className="spinner">
           <Spin size="large" />
         </Flex>
@@ -127,14 +129,15 @@ export default function Home() {
         <>
           <div className="movie-list">
             {movies.map(movie => (
-              <Link key={movie.id} to={`/movie/${movie.id}`}>
-                <MovieCard key={movie.id} movie_info={movie} />
+              <Link to={`/movie/${movie.id}`}>
+              <MovieCard movie_info={movie} loading={loading} />
               </Link>
             ))}
           </div>
-          <Pagination current={currentPage} total={1000000} onChange={handlePageChange} onShowSizeChange={(current, pageSize)=>setMoviesPerPage(pageSize)}/>
+          <Pagination current={currentPage} total={totalElements} onChange={handlePageChange} onShowSizeChange={(current, pageSize) => setMoviesPerPage(pageSize)} pageSize={moviesPerPage}/>
         </>
-      )}
+      )
+      }
     </div>
   );
 }
